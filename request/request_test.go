e7ea5cc47af17b11999bf28c4a738d45719da77b
Soren Mathiasen <sorenm@mymessages.dk>
2025-10-21 14:02:41 +0200

    replace all methods with FillStruct

--- request/request_test.go
+++ request/request_test.go
@@ -6,193 +6,180 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"reflect"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseRequestWithQueryParametersTyped(t *testing.T) {
-	t.Run("[]string", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=value1,value2,value3",
-			},
+func TestFillStruct_QueryParameters(t *testing.T) {
+	e := echo.New()
+
+	t.Run("[]string comma-separated", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/?param1=value1,value2,value3", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param []string `in:"query=param1"`
 		}
 
-		res := []string{}
-		// detect if return type is a slice
-		ctx := echo.New().NewContext(req, nil)
-		err := GetQueryParam[[]string](ctx, "param1", &res)
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, []string{"value1", "value2", "value3"}, res)
+		assert.Equal(t, []string{"value1", "value2", "value3"}, params.Param)
 	})
-	t.Run("atlernate []string", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=value1&param1=value2&param1=value3",
-			},
+
+	t.Run("[]string repeated params", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/?param1=value1&param1=value2&param1=value3", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param []string `in:"query=param1"`
 		}
 
-		res := []string{}
-		// detect if return type is a slice
-		ctx := echo.New().NewContext(req, nil)
-		err := GetQueryParam[[]string](ctx, "param1", &res)
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, []string{"value1", "value2", "value3"}, res)
+		assert.Equal(t, []string{"value1", "value2", "value3"}, params.Param)
 	})
+
 	t.Run("string", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=value1",
-			},
+		req := httptest.NewRequest(http.MethodGet, "/?param1=value1", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param string `in:"query=param1"`
 		}
-		ctx := echo.New().NewContext(req, nil)
-		res := ""
-		err := GetQueryParam[string](ctx, "param1", &res)
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, "value1", res)
+		assert.Equal(t, "value1", params.Param)
 	})
+
 	t.Run("int", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=42",
-			},
+		req := httptest.NewRequest(http.MethodGet, "/?param1=42", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param int `in:"query=param1"`
 		}
-		ctx := echo.New().NewContext(req, nil)
-		res := 0
-		err := GetQueryParam[int](ctx, "param1", &res)
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, 42, res)
+		assert.Equal(t, 42, params.Param)
 	})
+
 	t.Run("bool", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=true",
-			},
+		req := httptest.NewRequest(http.MethodGet, "/?param1=true", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param bool `in:"query=param1"`
 		}
-		ctx := echo.New().NewContext(req, nil)
-		res := false
-		err := GetQueryParam[bool](ctx, "param1", &res)
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, true, res)
+		assert.Equal(t, true, params.Param)
 	})
+
 	t.Run("float64", func(t *testing.T) {
-		req := &http.Request{
-			URL: &url.URL{
-				Path:     "/path/to/resource",
-				RawQuery: "param1=42.42",
-			},
+		req := httptest.NewRequest(http.MethodGet, "/?param1=42.42", nil)
+		ctx := e.NewContext(req, nil)
+
+		type Request struct {
+			Param float64 `in:"query=param1"`
 		}
-		ctx := echo.New().NewContext(req, nil)
-		res := 0.0
-		err := GetQueryParam[float64](ctx, "param1", &res)
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, 42.42, res)
+		assert.Equal(t, 42.42, params.Param)
 	})
 }
 
-func TestParseRequestWithPathParameters(t *testing.T) {
-	req := &http.Request{
-		URL: &url.URL{
-			Path: "/path/to/resource/:param1/:param2",
-		},
-	}
-
+func TestFillStruct_PathParameters(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/path/value1/value2", nil)
 	ctx := echo.New().NewContext(req, nil)
 	ctx.SetParamNames("param1", "param2")
 	ctx.SetParamValues("value1", "value2")
 
-	res := ""
-	err := GetPathParams[string](ctx, "param1", &res)
+	type Request struct {
+		Param1 string `in:"path=param1"`
+		Param2 string `in:"path=param2"`
+	}
+
+	params := &Request{}
+	err := FillStruct(ctx, params)
 	assert.NoError(t, err)
-	assert.Equal(t, "value1", res)
-	err = GetPathParams[string](ctx, "param2", &res)
-	assert.NoError(t, err)
-	assert.Equal(t, "value2", res)
+	assert.Equal(t, "value1", params.Param1)
+	assert.Equal(t, "value2", params.Param2)
 }
 
-func TestGetBody(t *testing.T) {
-	tests := []struct {
-		name       string
-		body       string
-		want       interface{}
-		wantError  bool
-		statusCode int
-	}{
-		{
-			name:       "Get body with valid JSON",
-			body:       `{"name": "John Doe"}`,
-			want:       &struct{ Name string }{Name: "John Doe"},
-			wantError:  false,
-			statusCode: http.StatusOK,
-		},
-		{
-			name:       "Get body with invalid JSON",
-			body:       `{"name": "John Doe"`,
-			want:       nil,
-			wantError:  true,
-			statusCode: http.StatusBadRequest,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			// Create a new request with the test body.
-			req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(test.body)))
+func TestFillStruct_Body(t *testing.T) {
+	e := echo.New()
 
-			// Create a new echo context.
-			e := echo.New()
-
-			body := &struct{ Name string }{}
-			// Call the GetBody function.
-			err := GetBody(e.NewContext(req, nil), body)
-
-			// Check the results.
-			if test.wantError {
-				if err == nil {
-					t.Errorf("GetBody() did not return an error when it should have")
-				}
-			} else {
-				if err != nil {
-					t.Errorf("GetBody() returned an error when it should not have: %v", err)
-				}
-				if !reflect.DeepEqual(body, test.want) {
-					t.Errorf("GetBody() got = %v, want = %v", body, test.want)
-				}
-			}
-		})
-	}
-}
-
-func TestGetHeaderParams(t *testing.T) {
-	t.Run("GetHeaderParams", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodPost, "/", nil)
-		req.Header.Add("Content-Type", "application/json")
-
-		e := echo.New()
+	t.Run("valid JSON", func(t *testing.T) {
+		bodyJSON := `{"name":"John Doe"}`
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(bodyJSON)))
+		req.Header.Set("Content-Type", "application/json")
 		ctx := e.NewContext(req, nil)
 
-		res := ""
-		err := GetHeaderParams[string](ctx, "Content-Type", &res)
+		type Request struct {
+			Name string `in:"body=name"`
+		}
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
 		assert.NoError(t, err)
-		assert.Equal(t, "application/json", res)
+		assert.Equal(t, "John Doe", params.Name)
 	})
-	t.Run("GetHeaderParams with invalid key", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodPost, "/", nil)
-		req.Header.Add("Content-Type", "application/json")
 
-		e := echo.New()
+	t.Run("invalid JSON", func(t *testing.T) {
+		bodyJSON := `{"name":"John Doe"`
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(bodyJSON)))
+		req.Header.Set("Content-Type", "application/json")
 		ctx := e.NewContext(req, nil)
 
-		res := ""
-		_ = GetHeaderParams[string](ctx, "Invalid-Key", &res)
-		assert.Equal(t, "", res)
+		type Request struct {
+			Name string `in:"body=name"`
+		}
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
+		assert.Error(t, err)
+	})
+}
+
+func TestFillStruct_Header(t *testing.T) {
+	t.Run("valid header", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		ctx := echo.New().NewContext(req, nil)
+
+		type Request struct {
+			ContentType string `in:"header=Content-Type"`
+		}
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
+		assert.NoError(t, err)
+		assert.Equal(t, "application/json", params.ContentType)
+	})
+
+	t.Run("missing header", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		ctx := echo.New().NewContext(req, nil)
+
+		type Request struct {
+			ContentType string `in:"header=Content-Type"`
+		}
+
+		params := &Request{}
+		err := FillStruct(ctx, params)
+		assert.NoError(t, err)
+		assert.Equal(t, "", params.ContentType)
 	})
 }
